176593440023bc336a91ef7cac267cb04fcb3281
Débora Rossini <deborarmc2008@gmail.com>
2022-03-17 19:03:53 -0300

    Create AlgoritmoGenetico.java

---
+++ AlgoritmoGenetico.java
@@ -0,0 +1,19 @@
+//Classe principal 
+
+package algoritmogenetico;
+
+/**
+ *
+ * @author aluno
+ */
+public class AlgoritmoGenetico {
+    // FALTA IMPLEMENTAR, PERAÍ =)
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
